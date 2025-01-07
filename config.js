98690e2eae9e4c1da47043faaae7707a89a822b7
hbjjgg <instagramvoting42@gmail.com>
2025-01-07 17:51:13 +0100

    Update config.js

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ const settingsMenu = [
   ]
 DATABASE_URL = process.env.DATABASE_URL === undefined ? './bot.db' : process.env.DATABASE_URL;
 DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBUG);
-if (!(process.env.SESSION || process.env.SESSION_ID)) throw new Error("No session found, add session before starting bot")
+if (!(process.env.SESSION || process.env.SESSION_ID))"Raganork~2e82b8747dd1d1cf9b29769fde8418e2:71326c6c54776b624a74773d"
 module.exports = {
     VERSION: 'v4.0.0',
     ALIVE: process.env.ALIVE || "https://i.imgur.com/KCnoMM2.jpg Hey {sender}, I'm alive \n Uptime: {uptime}",
